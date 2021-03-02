881dab02d327f690ac9de852a51bd782f5d9ed61
Sarah Marshall <sr70079@gmail.com>
2021-03-02 14:39:19 -0500

    view employee added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const fs = require("fs");
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const { printTable } = require("console-table-printer");
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -24,56 +26,91 @@ const start = () => {
         name: 'start',
         type: 'list',
         message: 'What would you like to do?',
-        choices: ['View all employees', 'View all employees by department', 'View all employees by manager', 'Add employee', 'Remove employee', 'Update employee role', 'Update employee manager', 'View all roles', 'Add role', 'Remove role', 'View all departments', 'Add department', 'Remove department', 'Quit'],
+        choices: ['View all employees', 
+        'View all employees by department', 
+        'View all employees by manager', 
+        'Add employee', 
+        'Remove employee', 
+        'Update employee role', 
+        'Update employee manager', 
+        'View all roles', 
+        'Add role', 
+        'Remove role', 
+        'View all departments', 
+        'Add department', 
+        'Remove department', 
+        'Quit'],
       })
       .then((answer) => {
         // based on their answer, goes to choice made
-        if (answer.viewEmployee === 'View all employees') {
+        if (answer.start === 'View all employees') {
             viewEmployee();
-        } else if (answer.viewByDepartment === 'View all employees by department') {
+        } else if (answer.start === 'View all employees by department') {
             viewByDepartment();
-        } 
-        else if (answer.viewByDepartment === 'View all employees by department') {
+        }  else if (answer.start === 'View all employees by department') {
             viewByDepartment();
-          }
-        else if (answer.viewManager === 'View all employees by manager') {
+        }  else if (answer.start === 'View all employees by manager') {
             viewManager();
-        }
-        else if (answer.addEmployee === 'Add employee') {
+        } else if (answer.start === 'Add employee') {
             addEmployee();
-        }
-        else if (answer.removeEmployee === 'Remove employee') {
+        } else if (answer.start === 'Remove employee') {
             removeEmployee();
-        }
-        else if (answer.updateEmployeeRole === 'Update employee role') {
+        } else if (answer.start === 'Update employee role') {
             updateEmployeeRole();
-        }
-        else if (answer.updateEmployeeManager === 'Update employee manager') {
+        } else if (answer.start === 'Update employee manager') {
             updateEmployeeManager();
-        }
-        else if (answer.viewRoles === 'View all roles') {
+        } else if (answer.start === 'View all roles') {
             viewRoles();
-        }
-        else if (answer.addRole === 'Add role') {
+        } else if (answer.start === 'Add role') {
             addRole();
-        }
-        else if (answer.removeRole === 'Remove role') {
+        } else if (answer.start === 'Remove role') {
             removeRole();
-        }
-        else if (answer.viewAllDepartments === 'View all departments') {
+        } else if (answer.start === 'View all departments') {
             viewAllDepartments();
-        }
-        else if (answer.addDepartment === 'Add department') {
+        } else if (answer.start === 'Add department') {
             addDepartment();
-        }
-        else if (answer.removeDepartment === 'Remove department') {
+        } else if (answer.start === 'Remove department') {
             removeDepartment();
-        }
-        else {
+        } else {
             connection.end();
         }
       });
   };
+
+//  function to view table of employee
+  const viewEmployee = () => {
+
+        connection.query('SELECT * FROM employee', (err, data) => {
+            if(err) throw err;        
+
+                printTable(data);
+    
+                start();
+            }           
+    
+        );          
+      
+    };
+
+    const viewEmployee = () => {
+
+            
+      
+    };
+
+    const viewAllDepartments = () => {
+
+        connection.query('SELECT * FROM department', (err, data) => {
+            if(err) throw err;        
+
+                printTable(data);
+    
+                start();
+            }           
+    
+        );      
+    };
+  
   
 
 
