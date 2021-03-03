91449eee92ebe25a95247fb33a042ba2204497bd
Sarah Marshall <sr70079@gmail.com>
2021-03-03 16:19:15 -0500

    add and remove roles and employee working

--- index.js
+++ index.js
@@ -151,80 +151,104 @@ const viewByDepartment = () => {
 
 // view all employees by manager
 const viewManager = () => {
-
-    connection.query('select e.first_name as \'Employee First Name\', e.last_name as \'Employee Last Name\', m.first_name as \'Manager First Name\', m.last_name as \'Manager Last Name\' from employee as e left join employee as m on e.id = m.manager_id where m.id is not null order by m.first_name', (err, data) => {
+    connection.query('select e.first_name as \'Employee First Name\', e.last_name as \'Employee Last Name\', m.first_name as \'Manager First Name\', m.last_name as \'Manager Last Name\' from employee as e left join employee as m on e.manager_id = m.id where m.id is not null order by m.first_name', (err, data) => {
         if(err) throw err;    
             printTable(data);    
             start();
         }         
-    );     
-    
+    );      
 };
 
+//add new employee
 const addEmployee = () => {
-
     connection.query('SELECT * FROM role', (err, data) => {
         const allRoles = data.map((allRoles) => {
             return {
                 value: allRoles.id,
                 name: allRoles.title 
             }
+        });    
+        if (err) throw err; 
+        connection.query('SELECT CONCAT(first_name, " ", last_name) as \'name\', id FROM employee WHERE id in (select manager_id from employee)', (err, data) => {
+            const allManagers = data.map((employee) => {
+                return {
+                    value: employee.id,
+                    name: employee.name
+                }
+            });
+            if (err) throw err;
+            inquirer.prompt([
+                {
+                    name: 'first_name',
+                    type: 'input',
+                    message: 'What is employee\'s first name?',
+                },
+                {
+                    name: 'last_name',
+                    type: 'input',
+                    message: 'What is employee\'s last name?',
+                },
+                {
+                    name: 'role',
+                    type: 'list',
+                    message: 'What is employee\'s role?',
+                    choices: allRoles
+                },
+                {
+                    name: 'manager',
+                    type: 'list',
+                    message: 'Who is employee\'s manager?',
+                    choices: allManagers
+                }
+            ]).then((data) => {
+                connection.query(
+                    "INSERT INTO employee SET ?",
+                    {
+                        first_name: data.first_name,
+                        last_name: data.last_name,
+                        role_id: data.role,
+                        manager_id: data.manager,
+                    },
+                    (err) => {
+        
+                        if (err) throw err;
+                        console.log('Employee has been added successfully!')
+        
+                        start();
+                    }
+                )
+            });        
+        });     
+    });       
+};
+
+//remove existing employee
+const removeEmployee = () => {
+    connection.query('SELECT CONCAT(first_name, " ", last_name) as \'name\', id FROM employee', (err, data) => {
+        const allEmployees = data.map((allEmployees) => {
+            return {
+                value: allEmployees.id,
+                name: allEmployees.name,
+            }
         })
         if(err) throw err;
         inquirer.prompt([
             {
-                name: 'first_name',
-                type: 'input',
-                message: 'What is employee first name?',
-            },
-            {
-                name: 'last_name',
-                type: 'input',
-                message: 'What is employee last name?',
-            },
-            {
-                name: 'role',
+                name: 'removeEmployee',
                 type: 'list',
-                message: 'What is employee role?',
-                choices: allRoles
+                message: 'Which employee would you like to remove?',
+                choices: allEmployees
+
             },
-            {
-                name: 'manager',
-                type: 'list',
-                message: 'Who is employee manager?',
-                choices: [
-                    //list all managers here
-                ],
-            }
         ]).then((data) => {
             connection.query(
-                "INSERT INTO employee SET ?",
-                {
-                    first_name: data.first_name,
-                    last_name: data.last_name,
-                    role: data.role,
-                    manager: data.manager,
-                },
-                (error) => {
-    
-                    if (error) throw error;
-                    console.log('Employee has been added successfully!')
-    
-                    start();
-                }
-            )
-        });
-    })    
+                `DELETE FROM employee WHERE id=${data.removeEmployee}`);
+            if (err) throw err;
+            console.log('Employee has been removed successfully!')
 
-    
-    
-        
-    
-};
-
-const removeEmployee = () => {
-
-   
+            start();
+        });          
+    });   
 };
 
 const updateEmployeeRole = () => {
@@ -249,40 +273,82 @@ const viewRoles = () => {
     );        
 };
 
-const addRole = () => {
-
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "role",
-            message: "What is the role you would like to add?"
-        }
-    ]).then((data) => {
-        connection.query(
-            "INSERT INTO role SET ?",
-            {
-                title: data.title
-            },
-            (error) => {
-                
-                if (error) throw error;
-                printTable(data)
-                console.log("New role has been added.")
-
-                start();
+//add new role
+const addRole = () => {    
+    connection.query('SELECT * FROM department', (err, data) => {
+        const allDepartments = data.map((allDepartments) => {
+            return {
+                value: allDepartments.id,
+                name: allDepartments.name 
             }
-        )
-    });
+        })
+        if(err) throw err;
+        inquirer.prompt([
+            {
+                name: "roleTitle",
+                type: "input",                
+                message: "What is the title of the role you would like to add?"
+            },
+            {
+                name: "roleSalary",
+                type: "input",                
+                message: "What is the salary of the role you would like to add?"
+            },
+            {
+                name: 'roleDepartment',
+                type: 'list',
+                message: 'Which department would you like to add role to?',
+                choices: allDepartments
 
-        
+            },
+        ]).then((data) => {
+            connection.query(
+                "INSERT INTO role SET ?",
+                {
+                    title: data.roleTitle,
+                    salary: data.roleSalary,
+                    department_id: data.roleDepartment 
+                },
+                (err) => {
+                    
+                    if (err) throw err;
+                    console.log("New role has been added successfully!")
     
+                    start();
+                }
+            )
+        });          
+    });      
 };
 
+//remove existing role
 const removeRole = () => {
+    connection.query('SELECT * FROM role', (err, data) => {
+        const allRoles = data.map((allRoles) => {
+            return {
+                value: allRoles.id,
+                name: allRoles.title 
+            }
+        });
+        if(err) throw err;
+        inquirer.prompt([
+            {
+                name: 'removeRole',
+                type: 'list',
+                message: 'Which role would you like to remove?',
+                choices: allRoles
 
-        
-    
-};
+            },
+        ]).then((data) => {
+            connection.query(
+                `DELETE FROM role WHERE id=${data.removeRole}`);
+            if (err) throw err;
+            console.log('Role has been removed successfully!')
+
+            start();
+        });          
+    })    
+}; 
 
 // view all departments of employees
 const viewAllDepartments = () => {
